737a1f407a537653b32a2f59f15bb8d08bb046de
Esteban I. Ruiz Moreno <exio4.com@gmail.com>
2016-02-02 13:37:50 -0300

    remove useless parameter (and the multi-language support which didn't make sense anyway)

--- src/Main.hs
+++ src/Main.hs
@@ -21,28 +21,14 @@ import           System.FilePath.Posix
 
 import           Control.Exception
 
-data Lang = Haskell
-    deriving (Show,Eq,Ord)
-
 data CompilerError = CompilerError !Int !String !String
     deriving (Show,Eq,Typeable)
 instance Exception CompilerError
-
-readLang x | let lower = map toLower x
-           , (lower == "haskell" || lower == "hs")
-           = Just Haskell
-readLang _ = Nothing
 
 ghc file output = do (exc, out, err) <- readProcessWithExitCode "ghc" ["-O2", "-o", output, file] ""
                      case exc of
                           ExitSuccess{} -> return ()
                           ExitFailure n -> throwIO (CompilerError n out err)
-
-fromExitCode (ExitSuccess{}) = True
-fromExitCode (ExitFailure{}) = False
-
-compilers :: Map Lang (String -> String -> IO ())
-compilers = M.fromList [(Haskell, ghc)]
 
 hashFile :: FilePath -> IO BS.ByteString
 hashFile = fmap hashlazy . BSL.readFile
@@ -51,8 +37,8 @@ hashFile = fmap hashlazy . BSL.readFile
 oldName :: String -> String -> String
 oldName mid file = takeDirectory file </> "." ++ mid ++ "." ++ takeFileName file
 
-go :: (String -> String -> IO ()) -> String -> [String] -> IO ()
-go compile file args =
+worker :: String -> [String] -> IO ()
+worker file args =
         do { new_hash <- hashFile file
            ; old_hash <- try (BS.readFile (oldName "hash" file))
            ; case old_hash of
@@ -64,20 +50,17 @@ go compile file args =
         } `catch` \(CompilerError n out err) -> do
             putStrLn  out
             putStrLn  err
-            putStrLn $ "RET: " ++ show n
+            exitWith (ExitFailure n)
           `catch` \(e :: SomeException) -> do
               print e
     where update new_hash = do
-                compile file (oldName "bin" file)
+                ghc file (oldName "bin" file)
                 BS.writeFile (oldName "hash" file) new_hash
-          run = waitForProcess  =<< runProcess (oldName "bin" file) args Nothing Nothing Nothing Nothing Nothing
+          run = waitForProcess =<< runProcess (oldName "bin" file) args Nothing Nothing Nothing Nothing Nothing
 
 main :: IO ()
 main = do { x <- getArgs
           ; case x of
-                (lang : file : args)
-                    | Just lang'   <- readLang lang
-                    , Just compile <- M.lookup lang' compilers
-                    -> go compile file args
+                (file : args) -> worker file args
                 _ -> putStrLn "Invalid parameters"
           }
